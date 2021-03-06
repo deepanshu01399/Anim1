bb911d4d677a02960e7c132043947876c67e7219
deepanshu01399 <49563880+deepanshu01399@users.noreply.github.com>
2021-03-06 10:06:43 +0530

    +location,home,setting icon animation using shapshitter

---
+++ app/src/main/java/com/deepanshu/anim1/activity/Notes.java
@@ -0,0 +1,16 @@
+package com.deepanshu.anim1.activity;
+
+public class Notes{
+
+    //  On animated vector images/icons
+
+    /*1.go to shapshitter or on https://medium.com/@naththeprince/delightful-animations-in-android-d6e9c62a23d3  (you will the refrence how to make the animated icons
+    2.in shapshiiter we can change the color, translate,scaling the icons..
+
+   a.for  color(): use fillcolor
+    the are use inside the Group (animation part:like transalte and scalling)
+    b. for translate : use translatex and transalte Y(edhar udhar move karanaa
+    c. for scalling the icons: (chote -bade karna ):
+
+*/
+}
